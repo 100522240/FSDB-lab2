aca435b03bcb4fe619b9ff93630fd114fa62dbf8
100522240@alumnos.uc3m.es <100522240@alumnos.uc3m.es>
2025-03-29 15:18:11 +0100

    esto es raro, creo que va pero ni idea

--- ReportsWorkers.sql
+++ ReportsWorkers.sql
@@ -14,26 +14,31 @@ floor((max(nvl(a.taskdate, sysdate)) - nvl(min(a.taskdate), sysdate)) / 365) as 
 /*To obtain the number of stops per active year we have to count the number of not null taskdates assigned to the driver
 and then divide it by the number of active years. In case that the active years is 0, we would get an error. Thus we have made a case statement where will divide the 
 taskdates by the number of active years if it is different than zero, otherwise, we will divide by one*/
-count(a.taskdate) /
+count(distinct a.taskdate) /
     case
         when floor((max(nvl(a.taskdate, sysdate)) - min(nvl(a.taskdate, sysdate))) / 365) != 0 then
-        floor((max(nvl(a.taskdate, sysdate)) - min(a.taskdate)) / 365)
+        floor((max(nvl(a.taskdate, sysdate)) - min(nvl(a.taskdate, sysdate))) / 365)
         else 1
     end as num_stops_per_year,
 /*For calculating the number of loans per year we have to do the same thing as with the number of stops, but
 instead of counting the taskdates, we will count the signatures*/
-count(l.signature) /
+count(distinct case when l.return is null then l.signature else null end) /
     case
         when floor((max(nvl(a.taskdate, sysdate)) - min(nvl(a.taskdate, sysdate))) / 365) != 0 then
         floor((max(nvl(a.taskdate, sysdate)) - min(a.taskdate)) / 365)
         else 1
-    end as num_stops_per_year,
+    end as num_unreturned_loans_per_year,
 /*To obtain the percentage of unreturned loans we have to count those that have null in the return column and then divide by the number of loans, and then multiply by 100*/
 round(
-    (count(case when l.return is null then 1 else 0 end) / count(l.signature)) * 100, 2
-) as percentage_unreturned loans
+    case 
+        when count(distinct l.signature) = 0 then 0  -- Avoid division by zero
+        else (count(distinct case when l.return is null then l.signature end) * 100.0) 
+             / count(distinct l.signature) 
+    END, 
+    2
+) AS percentage_unreturned_loans
 from drivers d
 left outer join assign_drv a on d.passport = a.passport
 left outer join services s on a.passport = s.passport
-left outer join loans l on l.taskdate = s.taskdate, l.town = s.town, l.province = s.province
+left outer join loans l on l.stopdate = s.taskdate and l.town = s.town and l.province = s.province
 group by d.fullname, d.birthdate, d.cont_start, d.cont_end;
