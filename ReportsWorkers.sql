21571ff02b352e7a962d288ebfb2e84289f38946
100522240@alumnos.uc3m.es <100522240@alumnos.uc3m.es>
2025-03-29 13:27:03 +0100

    XD

---
+++ ReportsWorkers.sql
@@ -0,0 +1,39 @@
+select d.fullname,
+/* Obtain the age of the driver by substracting the driver's birhtdate to the actual date and dividing by 365. 
+For obtaining an integer value smaller or equal than the obtained result, we will use floor()
+*/
+floor((sysdate - d.birthdate) / 365) as age, 
+/*--For obtaining the seniority contracted we have to substract the contract start date
+to the contract end date (or the actual date in case that it is null)
+*/
+floor((nvl(d.cont_end, sysdate) - d.cont_start) / 365) as seniority_contracted,
+/*To obtain the active years, we have taken into account the differnece between taskdates (columnn from assign_drv table).
+If a driver has null taskdates, then we replace them with the actual date. If they have more, we will obtain the earliest and latest taskdates
+and we then substract them to obtain the number of days and finally divide by 365*/ 
+floor((max(nvl(a.taskdate, sysdate)) - nvl(min(a.taskdate), sysdate)) / 365) as active_years,
+/*To obtain the number of stops per active year we have to count the number of not null taskdates assigned to the driver
+and then divide it by the number of active years. In case that the active years is 0, we would get an error. Thus we have made a case statement where will divide the 
+taskdates by the number of active years if it is different than zero, otherwise, we will divide by one*/
+count(a.taskdate) /
+    case
+        when floor((max(nvl(a.taskdate, sysdate)) - min(nvl(a.taskdate, sysdate))) / 365) != 0 then
+        floor((max(nvl(a.taskdate, sysdate)) - min(a.taskdate)) / 365)
+        else 1
+    end as num_stops_per_year,
+/*For calculating the number of loans per year we have to do the same thing as with the number of stops, but
+instead of counting the taskdates, we will count the signatures*/
+count(l.signature) /
+    case
+        when floor((max(nvl(a.taskdate, sysdate)) - min(nvl(a.taskdate, sysdate))) / 365) != 0 then
+        floor((max(nvl(a.taskdate, sysdate)) - min(a.taskdate)) / 365)
+        else 1
+    end as num_stops_per_year,
+/*To obtain the percentage of unreturned loans we have to count those that have null in the return column and then divide by the number of loans, and then multiply by 100*/
+round(
+    (count(case when l.return is null then 1 else 0 end) / count(l.signature)) * 100, 2
+) as percentage_unreturned loans
+from drivers d
+left outer join assign_drv a on d.passport = a.passport
+left outer join services s on a.passport = s.passport
+left outer join loans l on l.taskdate = s.taskdate, l.town = s.town, l.province = s.province
+group by d.fullname, d.birthdate, d.cont_start, d.cont_end;
