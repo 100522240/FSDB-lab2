069710d9e04e7b2b25dae9ef67eb5d5bf4d6b866
z4na14 <zanajorgesaghin@gmail.com>
2025-04-01 12:17:00 +0200

    uwu

--- Foundicu.sql
+++ Foundicu.sql
@@ -3,6 +3,10 @@ create or replace package foundicu as
     procedure insert_loan(l_signature char);
     procedure insert_reservation(isbn varchar2,res_date date);
     procedure record_books_ret(siganture char);
+
+    current_user VARCHAR2(10);
+    PROCEDURE set_current_user(p_user IN VARCHAR2);
+    FUNCTION get_current_user RETURN VARCHAR2;
 end;
 /
 
@@ -49,82 +53,82 @@ create or replace package body foundicu as
 
             --If v_reservation_count is bigger than 0, update the type and the stopdate of the loan
             if v_reservation_count > 0 then
-            update loans
-            set stopdate = sysdate, type = 'L'
-            where signature = l_signature and user_id = v_user_id;
-            
+                update loans
+                set stopdate = sysdate, type = 'L'
+                where signature = l_signature and user_id = v_user_id;
+                
             --Otherwise, check if there are loans available in two weeks
             else
 
-            --First check if there is a copy available
-            select count(*) into v_copy_available
-            from copies
-            where signature = l_signature;
+                --First check if there is a copy available
+                select count(*) into v_copy_available
+                from copies
+                where signature = l_signature;
 
-            if v_copy_available > 0 then
-                raise_application_error(-20002, 'Copy not available');
-            end if;
+                if v_copy_available > 0 then
+                    raise_application_error(-20002, 'Copy not available');
+                end if;
             
-            --Now check if there are loans for that copy in the span of two weeks from today
-            select count(*) into v_loans
-            from loans
-            where signature = l_signature
-            and type = 'L'
-            and (
-                ((stopdate between sysdate and sysdate + 14) and (return is null or return > sysdate + 14)) or
-                ((stopdate < sysdate or stopdate > sysdate + 14) and (return is null or return > sysdate + 14))
-            );
-            
-            --If the counter returns a value bigger than 0, this means that the book is loaned and will be loaned within the span of 2 weeks
-            if v_loans > 0 then 
-                raise_application_error(-20003, 'Book currently loaned and will be loaned within 2 weeks');
-            end if;
+                --Now check if there are loans for that copy in the span of two weeks from today
+                select count(*) into v_loans
+                from loans
+                where signature = l_signature
+                and type = 'L'
+                and (
+                    ((stopdate between sysdate and sysdate + 14) and (return is null or return > sysdate + 14)) or
+                    ((stopdate < sysdate or stopdate > sysdate + 14) and (return is null or return > sysdate + 14))
+                );
+                
+                --If the counter returns a value bigger than 0, this means that the book is loaned and will be loaned within the span of 2 weeks
+                if v_loans > 0 then 
+                    raise_application_error(-20003, 'Book currently loaned and will be loaned within 2 weeks');
+                end if;
 
-            --Now check if the user has reached the maximum amount of loans available and if he/she is banned
-            select count(*) into v_loans_count
-            from loans
-            where user_id = v_user_id and type = 'L' and return is null;
-            --If v_loans_count is bigger than 2, raise an error
-            if v_loans_count > 2 then
-                raise_application_error(-20004, 'User has reached the loan limit');
-            --If the user is banned, raise an error
-            elsif v_ban_up2 is not null and v_ban_up2 > sysdate then
-                raise_application_error(-20005, 'The user is banned of using the library services');
-            end if;
+                --Now check if the user has reached the maximum amount of loans available and if he/she is banned
+                select count(*) into v_loans_count
+                from loans
+                where user_id = v_user_id and type = 'L' and return is null;
+                --If v_loans_count is bigger than 2, raise an error
+                if v_loans_count > 2 then
+                    raise_application_error(-20004, 'User has reached the loan limit');
+                --If the user is banned, raise an error
+                elsif v_ban_up2 is not null and v_ban_up2 > sysdate then
+                    raise_application_error(-20005, 'The user is banned of using the library services');
+                end if;
 
-            --If everything has worked correctly, inserts a new row in the loans table
-            --Store in local variables the town and province of the user
-            select town, province into v_user_town, v_user_province
-            from users
-            where user_id = v_user_id;
+                --If everything has worked correctly, inserts a new row in the loans table
+                --Store in local variables the town and province of the user
+                select town, province into v_user_town, v_user_province
+                from users
+                where user_id = v_user_id;
 
-            --Store in a variable a counter with the services that match the sysdate and take place in the same town and province as the ones the user is in
-            select count(*) into v_count_services
-            from services
-            where town = v_user_town and province = v_user_province and taskdate = sysdate;
-
-            --If there are no services, then raise an error. Otherwise, continue the execution.
-            if v_count_services = 0 then
-                raise_application_error(-20006, 'No services found');
-            else
-                select taskdate into v_taskdate
+                --Store in a variable a counter with the services that match the sysdate and take place in the same town and province as the ones the user is in
+                select count(*) into v_count_services
                 from services
                 where town = v_user_town and province = v_user_province and taskdate = sysdate;
+
+                --If there are no services, then raise an error. Otherwise, continue the execution.
+                if v_count_services = 0 then
+                    raise_application_error(-20006, 'No services found');
+                else
+                    select taskdate into v_taskdate
+                    from services
+                    where town = v_user_town and province = v_user_province and taskdate = sysdate;
+                end if;
+
+                select into v_hour to_number(to_char(sysdate, 'HH24'))
+                from dual;
+
+                select into v_minutes to_number(to_char(sysdate, 'MI'))
+                from dual;
+
+                v_time := v_hour * 60 + v_minutes;
+
+                insert into loans(signature, user_id, stopdate, town, province, type, time)
+                values (l_signature, v_user_id, v_taskdate, v_user_town, v_user_province, 'L', v_time);
+            
             end if;
-
-            select into v_hour to_number(to_char(sysdate, 'HH24'))
-            from dual;
-
-            select into v_minutes to_number(to_char(sysdate, 'MI'))
-            from dual;
-
-            v_time := v_hour * 60 + v_minutes;
-
-            insert into loans(signature, user_id, stopdate, town, province, type, time)
-            values (l_signature, v_user_id, v_taskdate, v_user_town, v_user_province, 'L', v_time);
-            end if;
-
-        end insert_loan;
+    end insert_loan;
 
     --Complete description of insert_reservation procedure
     procedure insert_reservation(isbn varchar2,res_date date) is
@@ -153,15 +157,20 @@ create or replace package body foundicu as
             raise_application_error(-20008, 'The user has reached the borrow limit');
         end if;
 
-        
+        -- TODO: NOT FINISHED
 
+    end insert_reservation;
 
-
-
-
-
-
-
-
-       
-
+    -- Replace current user 
+    PROCEDURE set_current_user(p_user IN VARCHAR2) IS
+        BEGIN current_user := p_user; END;
+    
+    -- Retrieve current user
+    FUNCTION get_current_user RETURN VARCHAR2 IS BEGIN 
+        -- If no user is manually defined, use the predefined one
+        if current_user is NULL then
+            return USER;
+        else 
+            RETURN current_user;
+        end if;    
+    END get_current_user;
---
+++ external_desing.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE VIEW my_data AS
+    SELECT user_id, first_name, last_name, address, email, phone
+    FROM Users
+    WHERE user_id = foundicu.get_current_user();
+
+
+
+CREATE OR REPLACE VIEW my_loans AS
+    SELECT l.signature, l.stopdate, l.return, 
+        p.text, p.post_date, p.likes, p.dislikes
+    FROM Loans l
+    LEFT JOIN Posts p ON l.user_id = p.user_id
+    WHERE l.user_id = foundicu.get_current_user();
+
+CREATE OR REPLACE TRIGGER trg_update_my_loans
+    INSTEAD OF UPDATE ON my_loans
+    FOR EACH ROW
+    BEGIN
+        IF :NEW.post_text IS NOT NULL THEN
+            -- Update or insert post
+            MERGE INTO Posts p
+            USING (SELECT :NEW.loan_id AS loan_id FROM dual) src
+            ON (p.loan_id = src.loan_id)
+            WHEN MATCHED THEN
+                UPDATE SET p.post_text = :NEW.post_text, 
+                        p.post_date = SYSDATE
+            WHEN NOT MATCHED THEN
+                INSERT (loan_id, post_text, post_date, likes, dislikes)
+                VALUES (:NEW.loan_id, :NEW.post_text, SYSDATE, 0, 0);
+        END IF;
+    END;
+    /
+
+
+
+CREATE OR REPLACE VIEW my_reservations AS
+    SELECT r.reservation_id, r.isbn, r.reserve_date, r.expiry_date
+    FROM Reservations r
+    WHERE r.user_id = foundicu.get_current_user();
+
+CREATE OR REPLACE TRIGGER trg_manage_my_reservations
+    INSTEAD OF INSERT OR UPDATE OR DELETE ON my_reservations
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            -- Check book availability before inserting
+            INSERT INTO Reservations (reservation_id, user_id, isbn, reserve_date, expiry_date)
+            VALUES (:NEW.reservation_id, foundicu.get_current_user(), :NEW.isbn, :NEW.reserve_date, :NEW.expiry_date);
+        ELSIF UPDATING THEN
+            -- Allow date changes only if book is available
+            UPDATE Reservations 
+            SET reserve_date = :NEW.reserve_date, 
+                expiry_date = :NEW.expiry_date
+            WHERE reservation_id = :OLD.reservation_id;
+        ELSIF DELETING THEN
+            DELETE FROM Reservations WHERE reservation_id = :OLD.reservation_id;
+        END IF;
+    END;
+    /
