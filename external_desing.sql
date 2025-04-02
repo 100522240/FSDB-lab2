e9506b8c9a4099758e40c279c87f21f5292c5488
100522240@alumnos.uc3m.es <100522240@alumnos.uc3m.es>
2025-04-02 11:03:22 +0200

    No se que cojones esta mal

--- external_desing.sql
+++ external_desing.sql
@@ -41,8 +41,9 @@ CREATE OR REPLACE VIEW my_reservations AS
     l.user_id,
     c.isbn,
 
-    FROM loans l JOIN copies c on l.signature = c.signature
-    AND l.user_id = foundicu.get_current_user()
+    FROM loans l
+    JOIN copies c on l.signature = c.signature
+    WHERE l.user_id = foundicu.get_current_user()
     AND l.type = 'R';
 
 CREATE OR REPLACE TRIGGER trg_manage_my_reservations
